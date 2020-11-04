a8f614cfbf22f8bac5281706b01f0f11e5bb4b6f
pecovnik <zp8358@student.uni-lj.si>
2020-11-04 11:24:05 +0100

    moved aws config data to separate file

--- src/main/java/fri/uni_lj/si/fileUploadService/config/AmazonConfig.java
+++ src/main/java/fri/uni_lj/si/fileUploadService/config/AmazonConfig.java
@@ -6,17 +6,23 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AmazonConfig {
 
+    @Value("${aws.public_access_key}")
+    private String accessKey;
+    @Value("${aws.secret_access_key}")
+    private String secretKey;
+
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIAJAMW6S3EYDRKM5BQ",
-                "OKMhrOSvFp8qolUj3tIMbQKHpEed7Y+6z1tqaqYo"
+                accessKey,
+                secretKey
         );
 
         return AmazonS3ClientBuilder
