4b2491e493841897925647f5447e7eb4045106fe
pecovnik <zp8358@student.uni-lj.si>
2020-11-30 14:53:00 +0100

    test push to k8s

--- src/main/java/fri/uni_lj/si/fileUploadService/FileUploadServiceApplication.java
+++ src/main/java/fri/uni_lj/si/fileUploadService/FileUploadServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class FileUploadServiceApplication {
@@ -11,5 +12,4 @@ public class FileUploadServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FileUploadServiceApplication.class, args);
 	}
-
 }
