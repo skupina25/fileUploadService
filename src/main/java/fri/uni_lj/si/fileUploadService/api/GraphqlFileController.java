58e83d60ee423fd46a43d621e723e80fd42dd117
pecovnik <zp8358@student.uni-lj.si>
2021-01-07 16:18:49 +0100

    Fixed graphql endpoint

--- src/main/java/fri/uni_lj/si/fileUploadService/api/GraphqlFileController.java
+++ src/main/java/fri/uni_lj/si/fileUploadService/api/GraphqlFileController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("v1/graphql/files")
+@RequestMapping("v1/files/graphql")
 @CrossOrigin("*")
 public class GraphqlFileController {
     @Autowired
